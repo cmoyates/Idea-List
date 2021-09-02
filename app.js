6ac8f71d2b615fa77950628954c677fea00096aa
Cristopher Yates <cmoyates@gmail.com>
2021-09-02 02:30:45 -0230

    Added a request parameter

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const cors = require("cors");
 
 app.use(cors());
 
-app.use("/", async (req, res) => {
-    console.log("Success");
-    res.send("Success");
+app.use("/:arg", async (req, res) => {
+    const {arg} = req.params;
+    const msg = `Success, you said: ${arg}`
+    console.log(msg);
+    res.send(msg);
 })
 
 module.exports = app;
