f2eb6b7adaa4f98d83d4e8d971895516f82bd7bd
Cristopher Yates <cmoyates@gmail.com>
2021-09-02 09:39:46 -0230

    Trying something

--- app.js
+++ app.js
@@ -7,13 +7,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/suggestions", suggestion_router);
 
-/*app.use("/suggestions/:user/:suggestion", async (req, res) => {
-    const {user, suggestion} = req.params;
-    const msg = `Success, ${user} said: ${suggestion}`
-    console.log(msg);
-    res.send(msg);
-})*/
-
 app.use("/", async (req, res) => {
     res.send("Still working!");
 })
--- index.js
+++ index.js
@@ -1,4 +1,60 @@
 const app = require("./app");
+
+const http = require('http');
+const server = http.createServer(app);
+const socketio = require('socket.io');
+const io = socketio(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+
+
+
+io.on('connection', (socket) => {
+
+    // When a user joins a room
+    socket.on('join', (callback) => {
+
+        if (error) return callback(error);
+
+        socket.join(user.room);
+        console.log("User connected");
+
+        callback();
+    })
+
+
+    // When a new suggestion gets added
+    socket.on('newSuggestion', (profile_id) => {
+        io.emit('suggestionAdded');
+    })
+
+
+    // When a user disconnects
+    socket.on('disconnect', () => {
+        //const user = removeUser(socket.id);
+
+        /*if (user) {
+            io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
+        }*/
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
